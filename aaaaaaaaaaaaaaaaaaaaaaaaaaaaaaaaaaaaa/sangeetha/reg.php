02773536deea820475e523521d4aa0cfb7d7ffd1
SriHarish <harishkumarwebsite@gmail.com>
2022-08-23 10:31:34 +0530

    updated

--- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/sangeetha/reg.php
+++ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/sangeetha/reg.php
@@ -1,4 +1,12 @@
 <?php
+$h1="localhost";
+$u='root';
+$p='';
+$db="online_examination_with_security";
+if($conn==false){
+    die('could not connect:'.mysqli_error());
+} 
+
 $instid=$_POST['instid'];
 $instname=$_POST['instaname'];
 $dob=$_POST['dob'];
@@ -13,14 +21,8 @@ $cla=$_POST['cla'];
 $un=$_POST['un'];
 $youremail=$_POST['youremail'];
 $confirm-psw=$_POST['confirm-psw'];
-$h1="localhost";
-$u='root';
-$p='';
-$db="online_examination_with_security";
 
-if($conn==false){
-    die('could not connect:'.mysqli_error());
-}
+
 $v1='insert into instructor(id,instructor_id,dob,phone_number,file,favques,department,post,batch,gender,class,username,gmail,password) values ('$instid','$instname','$dob','$phone','$inputGroupFile01','$favques','$dept','$postdes','$batch','$gend','$cla','$un','$youremail','$confirm-psw')';
 mysqli_query($conn,$v1);
 mysqli_rollback($conn);
