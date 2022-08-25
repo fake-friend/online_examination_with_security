924c4f4604f08f6cc94b40dd91b50966530898d7
SriHarish <harishkumarwebsite@gmail.com>
2022-08-25 10:34:32 +0530

    php inserted

---
+++ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/hepsy/register.php
@@ -0,0 +1,35 @@
+<?php
+$h1="localhost";
+$u='root';
+$p='';
+$db="online_examination_with_security";
+if($conn==false){
+    die('could not connect:'.mysqli_error());
+} 
+
+$firstname=$_POST['firstname'];
+$fname=$_POST['fname'];
+$dob=$_POST['dob'];
+$phone=$_POST['phone'];
+$file=$_POST['file'];
+$favques=$_POST['favques'];
+$dept=$_POST['dept'];
+$postdes=$_POST['postdes'];
+$batch=$_POST['batch'];
+$gend=$_POST['gend'];
+$cla=$_POST['cla'];
+$un=$_POST['un'];
+$=$_POST['youremail'];
+$=$_POST[''];
+
+
+$v1='insert into instructor(id,instructor_id,dob,phone_number,file,favques,department,post,batch,gender,class,username,gmail,password) values ('$instid','$instname','$dob','$phone','$inputGroupFile01','$favques','$dept','$postdes','$batch','$gend','$cla','$un','$youremail','$confirm-psw')';
+mysqli_query($conn,$v1);
+mysqli_rollback($conn);
+echo"inserted";
+mysqli_close($conn);
+?>
+
+
+
+
