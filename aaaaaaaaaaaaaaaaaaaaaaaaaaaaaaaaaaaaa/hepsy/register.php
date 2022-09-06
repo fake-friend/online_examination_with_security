2b6248af1c5cc6bfac95d2d15cfc2a36755cb2f2
SriHarish <joannah1310@gmail.com>
2022-09-06 12:45:24 +0530

    inserted

--- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/hepsy/register.php
+++ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/hepsy/register.php
@@ -21,7 +21,7 @@ $batch=$_POST['batch'];
 $first1name=$_POST['first1name'];
 $your_email=$_POST['your_email'];
 $confirm_psw=$_POST['confirm-psw'];
-$v1="INSERT INTO student(name,fathers_name,dob,phonenumber,file,favqus,department,roll number,batch,shift,semester,username,gmail,password) VALUES ('$firstname','$fname','$dob','$phone','$file1','$favques','$dept','$rno','$shift','$batch','$first1name','$your_email','$confirm_psw')";
+$v1="INSERT INTO student(stuname,fathers_name,dob,phonenumber,files,favqus,department,roll_number,batch,shift,username,gmail,passwords) VALUES ('$firstname','$fname','$dob','$phone','$file1','$favques','$dept','$rno','$shift','$batch','$first1name','$your_email','$confirm_psw')";
 mysqli_query($conn,$v1);
 mysqli_rollback($conn);
 echo"inserted";
