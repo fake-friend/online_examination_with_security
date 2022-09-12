b87278abee02368d7b9f988031d481dfc75ab43c
iamsangeetha <sangeethasenthil2003@gmail.com>
2022-09-12 20:38:47 +0530

    login small change for instructor

--- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/sangeetha/instructorlog.php
+++ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/sangeetha/instructorlog.php
@@ -19,14 +19,14 @@ if($connection)
                     {
                         $_SESSION['instructor_name']=$row[0];
                         $_SESSION['gmail']=$email;
-                        $device_query="update admin set devices='1' where gmail='$email'";
+                        $device_query="update instructor set devices='1' where gmail='$email'";
                         mysqli_query($connection,$device_query);
                         mysqli_close($connection);
                         header("location:instlogin.php");
                     }
                     else
                     {
-                        header("location:slogin.html?message=you cannot login into multiple devices at a time!!!");
+                        //header("location:slogin.html?message=you cannot login into multiple devices at a time!!!");
                     }
                 }
                 else
