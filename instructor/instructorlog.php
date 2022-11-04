63d254636d22f6acd162b1de701d46b50df3771b
iamsangeetha <sangeethasenthil2003@gmail.com>
2022-11-04 15:52:31 +0530

    dot

--- instructor/instructorlog.php
+++ instructor/instructorlog.php
@@ -7,6 +7,7 @@ if($connection)
     {
             $email=$_POST['email'];
             $password=$_POST['pwd'];
+            
     
             $query='SELECT instructor_id, instructor_name, passwords, devices from instructor where gmail='.'"'.$email.'"';
             $result=mysqli_query($connection,$query);
@@ -17,12 +18,19 @@ if($connection)
                 {
                     if($row[3]==0)
                     {
+                         
+                        $date=date('m/d/y h:i:s',time());
                         $_SESSION['instructor_name']=$row[1];
+                        $iname=$_SESSION['instructor_name'];
                         $_SESSION['gmail']=$email;
+                        $imail=$_SESSION['gmail'];
                         $_SESSION['instructor_id']=$row[0];
                         $device_query="update instructor set devices='1' where gmail='$email'";
                         mysqli_query($connection,$device_query);
+                        $in="INSERT INTO instructortrackloguser(insname,email,logintime,logouttime) VALUES ('$iname','$imail','$date',NULL )";
+                        mysqli_query($connection,$in);
                         mysqli_close($connection);
+
                         echo "<script> location.href='http://localhost/online_examination_with_security/instructor/instructor_sidebar/index1.php'</script>";
                     }
                     else
--- instructor/logout.php
+++ instructor/logout.php
@@ -5,11 +5,14 @@ $p='';
 $db="online_examination_with_security";
 $connection=mysqli_connect($h1,$u,$p,$db);
 session_start();
+
 if($connection)
 {
     $gmail=$_SESSION['gmail'];
+    $date=date('d-m-y h:i:s',time());
     $device_query="update instructor set devices='0' where gmail='$gmail'";
     mysqli_query($connection,$device_query);
+    mysqli_query($connection, "UPDATE instructortrackloguser SET logouttime = $date WHERE email = '$gmail'");                                                                                                                                                                                                                                                              
     mysqli_close($connection);
 }
 session_unset();
